87ed8336d380504387aff2587ddc21e3c52a04d3
Axevalla <ssas25chza@axevalla.fhsk.se>
2025-11-10 11:46:44 +0100

    test

---
+++ Assets/laser.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    [SerializeField] private float Speed = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Speed * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/scriptplayer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class scriptplayer : MonoBehaviour
+{
+    [SerializeField] private float playerspeed = 3;
+    public float horizontalMovement;
+
+    public GameObject laserRed;
+    private int playerHealth = 3;
+   
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(0, -3.5f, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * playerspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        { 
+            transform.Translate(Vector3.left * playerspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserRed, this.transform.position, transform.rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Destroy(gameObject);
+        }
+
+
+
+
+
+
+
+    }
+}
